15a088cf7179fa5a039427250b6e4cb70046c586
3ay <lefrost.volkoff@yandex.ru>
2023-04-15 13:15:30 +0300

    update CategoryController, add checking parameters

--- src/main/java/com/example/tasklist/back/springboot/controller/CategoryController.java
+++ src/main/java/com/example/tasklist/back/springboot/controller/CategoryController.java
@@ -3,6 +3,9 @@ package com.example.tasklist.back.springboot.controller;
 import com.example.tasklist.back.springboot.entity.CategoryEntity;
 import com.example.tasklist.back.springboot.repo.CategoryRepository;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +25,16 @@ public class CategoryController {
         return categoryRepository.findAll();
     }
     @PostMapping("/addCategory")
-    public CategoryEntity addCategory (@RequestBody CategoryEntity category)
+    public ResponseEntity <CategoryEntity> addCategory (@RequestBody CategoryEntity category)
     {
-        return categoryRepository.save(category);
+        if (category.getId() != null && category.getId() != 0)
+        {
+            return new ResponseEntity("redundant param: id MUST be without value", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0)
+        {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 }
