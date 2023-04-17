4978f9a69ac142cd46e20d0819c873cf9be0600b
3ay <lefrost.volkoff@yandex.ru>
2023-04-17 22:25:25 +0300

    add methods findById and deleteById for CategoryController and PriorityController

--- src/main/java/com/example/tasklist/back/springboot/controller/CategoryController.java
+++ src/main/java/com/example/tasklist/back/springboot/controller/CategoryController.java
@@ -1,15 +1,19 @@
 package com.example.tasklist.back.springboot.controller;
 
 import com.example.tasklist.back.springboot.entity.CategoryEntity;
+import com.example.tasklist.back.springboot.entity.PriorityEntity;
 import com.example.tasklist.back.springboot.repo.CategoryRepository;
 
 import org.apache.coyote.Response;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/category")
@@ -46,5 +50,28 @@ public class CategoryController {
         } else {
             return new ResponseEntity("redundant param: id MUST be determined", HttpStatus.NOT_ACCEPTABLE);
         }
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<CategoryEntity> findById(@Valid @PathVariable Long id) {
+        CategoryEntity category;
+        try {
+            category = categoryRepository.findById(id).get();
+        } catch (NoSuchElementException exception) {
+            exception.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(category);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            exception.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/tasklist/back/springboot/controller/PriorityController.java
+++ src/main/java/com/example/tasklist/back/springboot/controller/PriorityController.java
@@ -3,16 +3,18 @@ package com.example.tasklist.back.springboot.controller;
 import com.example.tasklist.back.springboot.entity.CategoryEntity;
 import com.example.tasklist.back.springboot.entity.PriorityEntity;
 import com.example.tasklist.back.springboot.repo.PriorityRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @RestController
-@RequestMapping ("/priority") // базовый адрес
+@RequestMapping("/priority") // базовый адрес
 public class PriorityController {
 
     // доступ к данным из БД
@@ -34,11 +36,10 @@ public class PriorityController {
         return priorityRepository.findAll(); // JSON формат будет использоваться автоматически
 
     }
+
     @PostMapping("/addPriority")
-    public ResponseEntity <PriorityEntity> addPriority(@Valid @RequestBody PriorityEntity priority)
-    {
-        if(priority.getId() != null && priority.getId() != 0)
-        {
+    public ResponseEntity<PriorityEntity> addPriority(@Valid @RequestBody PriorityEntity priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
             return new ResponseEntity("redundant param: id MUST be without value", HttpStatus.NOT_ACCEPTABLE);
         }
         if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
@@ -46,6 +47,7 @@ public class PriorityController {
         }
         return ResponseEntity.ok(priorityRepository.save(priority));
     }
+
     @PutMapping("/updatePriority")
     public ResponseEntity<PriorityEntity> updateCategory(@Valid @RequestBody PriorityEntity priority) {
         if (priority.getId() != null && priority.getId() != 0) {
@@ -58,5 +60,26 @@ public class PriorityController {
         }
     }
 
+    @GetMapping("/id/{id}")
+    public ResponseEntity<PriorityEntity> findById(@Valid @PathVariable Long id) {
+        PriorityEntity priority;
+        try {
+           priority = priorityRepository.findById(id).get();
+        } catch (NoSuchElementException exception) {
+            exception.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priority);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        try {
+            priorityRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            exception.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
